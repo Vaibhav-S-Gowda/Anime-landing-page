df4656def1c7f4efa9005a2b007bc447ab7340f0
Vaibhav-S-Gowda <vaibhav.satheesh03@gmail.com>
2025-04-13 11:55:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import About from "./components/About";
+import Hero from "./components/hero";
+import Features from "./components/Features";
+import Story from "./components/Story";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+
+      <Hero />
+      <About />
+      <Features />
+      <Story />
+      <Contact />
+      <Footer />
+    </main>
+  );
+}
+
+export default App;
